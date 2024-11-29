906b078acae81a612ac6000acc7fb3ca12447dfc
Matthew Dudzinski <42253757+Resident-Fortress@users.noreply.github.com>
2024-11-28 22:44:26 -0600

    Initial Commit

---
+++ GeofenceExample/GeofenceExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeofenceExampleApp.swift
+//  GeofenceExample
+//
+//  Created by Matthew Dudzinski on 11/28/24.
+//
+
+import SwiftUI
+
+@main
+struct GeofenceExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
